08672cb7a8d98baa0d8b481dc23f69aa40a94b82
Alishba Kashif <alishbakashif98@gmail.com>
2026-01-03 07:29:15 -0800

    Add area of triangle in Basics folder

---
+++ Basics/Area-of-triangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	float b,h,Area;
+	cout<<"Enter the base:";
+	cin>>b;
+	cout<<"Enter the height:";
+	cin>>h;
+	Area=(b*h)/2;
+	cout<<"The Area of triangle is:"<<Area;
+	return 0;
+}
