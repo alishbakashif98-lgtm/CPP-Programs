38da692c2af45f58024072330df7deec9e15e022
Alishba Kashif <alishbakashif98@gmail.com>
2026-01-30 09:24:18 -0800

    Hello file added in folder loops

--- Loops/Hello.cpp
+++ Loops/Hello.cpp
@@ -1,13 +1,21 @@
+/*
+Program Definition:
+This program prints the word "Hello" 10 times using a while loop.
+A while loop executes a block of code repeatedly as long as a given condition is true.
+*/
+
 #include<iostream>
 using namespace std;
+
 int main(){
 
-int i = 1;
-while( i<=10)
-{
-cout<<"Hello"<<endl;
-i++;
-}
-return 0;
+    int i = 1; // initialize counter variable i to 1
 
+    while( i<=10) // loop runs while i is less than or equal to 10
+    {
+        cout<<"Hello"<<endl; // print "Hello" followed by a new line
+        i++; // increment i by 1 each iteration
+    }
+
+    return 0; // end of program
 }
